22aa273bafd49b5029b00937e92c356e099e4c24
Jasmin <jasminrasmussen3004@gmail.com>
2019-01-31 15:25:58 +0100

    added a music player and made it a prefab

---
+++ BotRun/Assets/DontDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+    public static DontDestroy instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+
+            Destroy(this.gameObject);
+        }
+    }
+
+}
