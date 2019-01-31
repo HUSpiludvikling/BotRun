68c062b96a8905e788f0d257baeedc91ae4c0823
Nikolaj <nikoil1@live.dk>
2019-01-31 15:03:00 +0100

    Changed Finish line script and added Win panel

--- BotRun/Assets/FinishLine.cs
+++ BotRun/Assets/FinishLine.cs
@@ -4,44 +4,46 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class FinishLine : MonoBehaviour {
+public class FinishLine : MonoBehaviour
+{
     private HighScore highScore;
     public GameObject panel;
     public Text ScoreText;
     public Text HighScoreText;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         panel.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             DisplayTime(other);
         }
     }
-    }
+
 
     private void DisplayTime(Collision other)
     {
-        Debug.Log("Hej");
         highScore = other.gameObject.GetComponent<HighScore>();
         Time.timeScale = 0f;
         panel.SetActive(true);
         Cursor.visible = true;
         float ScoreTime = Time.timeSinceLevelLoad;
-        ScoreText.text = "Score: " + ScoreTime.ToString("0.00");
+        ScoreText.text = "Score: " + ScoreTime.ToString("0.00") + " Seconds";
 
         highScore.SetHighScore(SceneManager.GetActiveScene().name, ScoreTime);
         float highScoreTime = highScore.GetHighScore(SceneManager.GetActiveScene().name);
 
-        HighScoreText.text = "Highscore: " + highScoreTime.ToString("0.00");
+        HighScoreText.text = "Highscore: " + highScoreTime.ToString("0.00") + " Seconds";
     }
 }
